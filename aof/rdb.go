e30352f06d958ec9c055401ebfcd49e079c66634
wixie <phantanlong645@gmail.com>
2026-04-01 22:56:30 +0800

    搭建rdb骨架流程

--- aof/rdb.go
+++ aof/rdb.go
@@ -1,1 +1,24 @@
 package aof
+
+import "go-Redis/database"
+
+func (p *Persister) GenerateRDB(rdbFilename string, dbSet *database.DBSet) error {
+	ctx, err := p.startGenerateRDB()
+	if err != nil {
+		return err
+	}
+	err = p.generateRDB(ctx, dbSet)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func (p *Persister) startGenerateRDB() (*RewriteCtx, error) {
+	return nil, nil
+}
+
+func (p *Persister) generateRDB(ctx *RewriteCtx, dbSet *database.DBSet) error {
+	return nil
+}
