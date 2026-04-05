697d63c64fdeb2e41dc836999a12a7483022fab1
wixie <phantanlong645@gmail.com>
2026-04-05 23:05:29 +0800

    写完取消订阅过程

--- pubsub/hub.go
+++ pubsub/hub.go
@@ -1,1 +1,19 @@
 package pubsub
+
+import (
+	"go-Redis/datastruct/dict"
+	"go-Redis/datastruct/lock"
+)
+
+type Hub struct {
+	subLocker *lock.Locks
+	//频道对应的连接
+	subs dict.Dict
+}
+
+func MakeHub() *Hub {
+	return &Hub{
+		subLocker: lock.Make(16),
+		subs:      dict.MakeConcurrent(4),
+	}
+}
--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -1,26 +1,14 @@
 package pubsub
 
 import (
-	"go-Redis/datastruct/dict"
 	"go-Redis/datastruct/list"
-	"go-Redis/datastruct/lock"
 	"go-Redis/interface/redis"
 	"go-Redis/redis/protocol"
 	"strconv"
 )
 
-type Hub struct {
-	subLocker *lock.Locks
-	//频道对应的连接
-	subs dict.Dict
-}
+var unSubscribeNothing = []byte("*3\r\n$11\r\nunsubscribe\r\n$-1\r\n:0\r\n")
 
-func MakeHub() *Hub {
-	return &Hub{
-		subLocker: lock.Make(16),
-		subs:      dict.MakeConcurrent(4),
-	}
-}
 func makeMsg(t string, channel string, code int64) []byte {
 	return []byte(
 		"*3\r\n$" + strconv.FormatInt(int64(len(t)), 10) + "\r\n" + t + "\r\n" +
@@ -43,8 +31,32 @@ func Subscribe(hub *Hub, c redis.Connection, args [][]byte) protocol.Reply {
 	return &protocol.NoReply{}
 
 }
+func UnSubscribe(hub *Hub, c redis.Connection, args [][]byte) protocol.Reply {
+	var channels []string
+	if len(args) > 0 {
+		channels = make([]string, len(args))
+		for i, b := range args {
+			channels[i] = string(b)
+		}
+	} else {
+		channels = c.GetChannels()
+	}
+	hub.subLocker.Locks(channels...)
+	defer hub.subLocker.UnLocks(channels...)
+	if len(channels) == 0 {
+		_, _ = c.Write(unSubscribeNothing)
+		return &protocol.NoReply{}
+	}
+	for _, channel := range channels {
+		if unSubscribe0(hub, c, channel) {
+			_, _ = c.Write(makeMsg("unsubscribe", channel, int64(c.SubsCount())))
+		}
+	}
+	return &protocol.NoReply{}
+
+}
 func subscribe0(hub *Hub, channel string, client redis.Connection) bool {
-	client.Subscribe(channel)
+
 	raw, ok := hub.subs.Get(channel)
 	var subscribers *list.LinkedList
 	if ok {
@@ -53,12 +65,34 @@ func subscribe0(hub *Hub, channel string, client redis.Connection) bool {
 		subscribers = list.Make()
 		hub.subs.Put(channel, subscribers)
 	}
-	if subscribers.Contains(func(a any) bool {
-		return a == client
-	}) {
+	alreadySubscribed := subscribers.Contains(func(a any) bool {
+		sub, ok := a.(redis.Connection)
+		return ok && sub == client
+	})
+	if alreadySubscribed {
 		return false
 	}
+	client.Subscribe(channel)
 	subscribers.Add(client)
+	return true
+}
+func unSubscribe0(hub *Hub, client redis.Connection, channel string) bool {
+	raw, ok := hub.subs.Get(channel)
+	if !ok {
+		return false
+	}
+	subscriber := raw.(*list.LinkedList)
+	removed := subscriber.RemoveAllByVal(func(a any) bool {
+		sub, ok := a.(redis.Connection)
+		return ok && sub == client
+	})
+	if removed == 0 {
+		return false
+	}
+	client.UnSubscribe(channel)
+	if subscriber.Len() == 0 {
+		hub.subs.Remove(channel)
+	}
 	return true
 
 }
